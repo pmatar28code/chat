61f031b125769e0a70cf93afd9139607008a016e
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2022-03-03 21:53:07 -0600

    good up to video 11

--- app/src/main/java/com/lionmgtllcagencyp/chat/MainActivity.kt
+++ app/src/main/java/com/lionmgtllcagencyp/chat/MainActivity.kt
@@ -2,10 +2,80 @@ package com.lionmgtllcagencyp.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.snackbar.Snackbar
+import com.lionmgtllcagencyp.chat.databinding.ActivityMainBinding
+import com.lionmgtllcagencyp.chat.databinding.FragmentMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    var sectionPagerAdapter: SectionPagerAdapter ?= null
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val inflater = LayoutInflater.from(this@MainActivity)
+        binding = ActivityMainBinding.inflate(inflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        sectionPagerAdapter = SectionPagerAdapter(supportFragmentManager)
+        binding.container.adapter = sectionPagerAdapter
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view,"this is a simple snackBar",Snackbar.LENGTH_SHORT)
+                .setAction("Action",null) .show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if(id == R.id.action_settings){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    inner class SectionPagerAdapter(fragmentManager:FragmentManager): FragmentPagerAdapter(fragmentManager) {
+        override fun getCount(): Int {
+            return 3
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return  PlaceHolderFragment.newIntent(position + 1)
+        }
+
+    }
+    class PlaceHolderFragment: Fragment(R.layout.fragment_main){
+        companion object{
+            private val ARG_SECTION_NUMBER = "Section Number"
+            fun newIntent(sectionNumber:Int):PlaceHolderFragment{
+                val fragment = PlaceHolderFragment()
+                val args = Bundle()
+                    args.putInt(ARG_SECTION_NUMBER,sectionNumber)
+                fragment.arguments = args
+                return fragment
+
+            }
+        }
+        lateinit var bindingFrag: FragmentMainBinding
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            bindingFrag = FragmentMainBinding.bind(view)
+            super.onViewCreated(view, savedInstanceState)
+
+            bindingFrag.sectionLabel.text = "Hello World from Section ${arguments?.getInt(ARG_SECTION_NUMBER)}"
+        }
+
     }
 }
